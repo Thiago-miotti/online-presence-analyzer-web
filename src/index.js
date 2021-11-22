051763aa37f28d514994db84f94f09eca061a582
Thiago <thiago.miotti10@gmail.com>
2021-11-22 10:37:19 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './global.css';
+import Company from './pages/Company';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Company />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
---
+++ src/pages/Company/index.jsx
@@ -0,0 +1,15 @@
+import "./style.css";
+import Sidebar from "../../components/sidebar";
+
+const Company = () => {
+  return (
+    <div className="company">
+      <Sidebar />
+      <div className="placeholder">
+        <h1>fnjdnfidnfnkd</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Company;
